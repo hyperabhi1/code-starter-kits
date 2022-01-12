e27f388f3e8dfe175f7008fd7b2901c44197eb03
hyperabhi1 <36052957+hyperabhi1@users.noreply.github.com>
2022-01-12 14:10:28 +0530

    Update Game.cs

--- bowling-ball/BowlingBall/Game.cs
+++ bowling-ball/BowlingBall/Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +8,54 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] rolls = new int[21];
+        int currentRoll = 0;
+
+        bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
         public void Roll(int pins)
         {
-            // Add your logic here. Add classes as needed.
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
         }
 
         public int GetScore()
         {
-            // Returns the final score of the game.
-            return 0;
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (IsStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            return score;
         }
     }
 }
